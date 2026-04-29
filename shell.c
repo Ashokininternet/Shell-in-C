2276e2681b783d8021178f8598e646f0fd375776
Ashok S.R <ashok.106sr@gmail.com>
2026-04-29 23:09:08 +0530

    Reading a line

--- shell.c
+++ shell.c
@@ -4,11 +4,52 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-void lsh_loop(void){
+#define LSH_RL_BUFSIZE 1024
+
+char *lsh_read_line()
+{
+  int bufsize = LSH_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * bufsize);
+  int c;
+  if (!buffer)
+  {
+    fprintf(stderr, "lsh:allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+  while (1)
+  {
+    c = getchar();
+    if (c == EOF || c == '\n')
+    {
+      buffer[position] = '\0';
+      return buffer;
+    }
+    else
+    {
+      buffer[position] = c;
+    }
+    position++;
+    if (position >= bufsize)
+    {
+      bufsize += LSH_RL_BUFSIZE;
+      buffer = realloc(buffer, bufsize);
+    }
+    if (!buffer)
+    {
+      fprintf(stderr, "lsh:allocation error\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+}
+
+void lsh_loop(void)
+{
   char *line;
   char **args;
   char status;
-  do {
+  do
+  {
     printf("> ");
     line = lsh_read_line();
     args = lsh_split_line(line);
@@ -16,10 +57,11 @@ void lsh_loop(void){
 
     free(line);
     free(args);
-  }while (status)
+  } while (status);
 }
 
-int main(int argc, char **argv){
-  
-  return EXIT_SUCCESS ;
+int main(int argc, char **argv)
+{
+
+  return EXIT_SUCCESS;
 }
