f6aa4b8d51714607c5cc93c17247dd17729b45bf
Ashok S.R <ashok.106sr@gmail.com>
2026-04-29 20:48:06 +0530

    Basic loop of a shell

--- shell.c
+++ shell.c
@@ -4,6 +4,22 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main(){
-  printf("hello");
+void lsh_loop(void){
+  char *line;
+  char **args;
+  char status;
+  do {
+    printf("> ");
+    line = lsh_read_line();
+    args = lsh_split_line(line);
+    status = lsh_execute(args);
+
+    free(line);
+    free(args);
+  }while (status)
+}
+
+int main(int argc, char **argv){
+  
+  return EXIT_SUCCESS ;
 }
