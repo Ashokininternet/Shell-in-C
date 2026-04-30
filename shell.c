b561386d6d5f6e26bae8522bb5b6416a63960393
Ashok S.R <ashok.106sr@gmail.com>
2026-04-30 22:18:47 +0530

    shell and processes

--- shell.c
+++ shell.c
@@ -8,7 +8,62 @@
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
 
-char *lsh_read_line()
+int lsh_launch(char **args){
+  pid_t pid, wpid;
+  int status;
+  pid = fork();
+
+  if (pid == 0)
+  {
+    if (execvp(args[0], args) == -1)
+    {
+      perror("lsh");
+    }
+    exit(EXIT_FAILURE);
+  }else if (pid < 0)
+  {
+    perror("lsh");
+  } else{
+    do
+    {
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+  return 1;
+}
+
+char **lsh_split_line(char *line)
+{
+  char bufsize = LSH_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char *));
+  char *token;
+  if (!tokens)
+  {
+    fprintf(stderr, "lsh:allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+  token = strtok(line, LSH_TOK_DELIM);
+  while (token != NULL)
+  {
+    tokens[position] = token;
+    position++;
+    if (position >= bufsize)
+    {
+      bufsize += LSH_TOK_BUFSIZE;
+      tokens = realloc(tokens, bufsize * sizeof(char *));
+      if (!tokens)
+      {
+        fprintf(stderr, "lsh:allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+    token = strtok(NULL, LSH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
+}
+
+char *lsh_read_line(void)
 {
   int bufsize = LSH_RL_BUFSIZE;
   int position = 0;
@@ -49,7 +104,7 @@ void lsh_loop(void)
 {
   char *line;
   char **args;
-  char status;
+  int status;
   do
   {
     printf("> ");
@@ -60,37 +115,6 @@ void lsh_loop(void)
     free(line);
     free(args);
   } while (status);
-}
-
-char **lsh_split_line(char *line)
-{
-  char bufsize = LSH_TOK_BUFSIZE, position = 0;
-  char **tokens = malloc(bufsize * sizeof(char *));
-  char *token;
-  if (!tokens)
-  {
-    fprintf(stderr, "lsh:allocation error\n");
-    exit(EXIT_FAILURE);
-  }
-  token = strtok(line, LSH_TOK_DELIM);
-  while (token != NULL)
-  {
-    tokens[position] = token;
-    position++;
-    if (position >= bufsize)
-    {
-      bufsize += LSH_TOK_BUFSIZE;
-      tokens = realloc(tokens, bufsize * sizeof(char *));
-      if (!tokens)
-      {
-        fprintf(stderr, "lsh:allocation error\n");
-        exit(EXIT_FAILURE);
-      }
-    }
-    token = strtok(NULL, LSH_TOK_DELIM);
-  }
-  tokens[position] = NULL;
-  return tokens;
 }
 
 int main(int argc, char **argv)
