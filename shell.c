86e735107aa1d657568aadf801840a9b3b9d62df
Ashok S.R <ashok.106sr@gmail.com>
2026-05-01 20:02:18 +0530

    add cd command to Shell Builtins

--- shell.c
+++ shell.c
@@ -8,7 +8,41 @@
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
 
-int lsh_launch(char **args){
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **atgs);
+
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+  &lsh_cd,
+  &lsh_help,
+  &lsh_exit
+};
+
+int lsh_num_builtins(){
+  return sizeof(builtin_str) / sizeof(char *);
+};
+
+int lsh_cd(char **args){
+  if (args[1] == NULL)
+  {
+    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+  }else{
+    if (chdir(args[1]) != 0)
+    {
+      perror("lsh");
+    }
+  }
+  return 1;
+};
+
+int lsh_launch(char **args)
+{
   pid_t pid, wpid;
   int status;
   pid = fork();
