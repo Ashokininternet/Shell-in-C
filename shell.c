f7111eacce33f31a0d94275a676567c2d761752e
Ashok S.R <ashok.106sr@gmail.com>
2026-05-04 22:04:19 +0530

    fix some position error for the execute function

--- shell.c
+++ shell.c
@@ -4,13 +4,13 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define LSH_RL_BUFSIZE 1024
-#define LSH_TOK_BUFSIZE 64
-#define LSH_TOK_DELIM " \t\r\n\a"
+#define ASH_RL_BUFSIZE 1024
+#define ASH_TOK_BUFSIZE 64
+#define ASH_TOK_DELIM " \t\r\n\a"
 
-int lsh_cd(char **args);
-int lsh_help(char **args);
-int lsh_exit(char **atgs);
+int ash_cd(char **args);
+int ash_help(char **args);
+int ash_exit(char **atgs);
 
 char *builtin_str[] = {
   "cd",
@@ -19,63 +19,43 @@ char *builtin_str[] = {
 };
 
 int (*builtin_func[]) (char **) = {
-  &lsh_cd,
-  &lsh_help,
-  &lsh_exit
+  &ash_cd,
+  &ash_help,
+  &ash_exit
 };
 
-int lsh_num_builtins(){
+int ash_num_builtins(){
   return sizeof(builtin_str) / sizeof(char *);
 };
 
-int lsh_cd(char **args){
+int ash_cd(char **args){
   if (args[1] == NULL)
   {
-    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+    fprintf(stderr, "ash: expected argument to \"cd\"\n");
   }else{
     if (chdir(args[1]) != 0)
     {
-      perror("lsh");
+      perror("ash");
     }
   }
   return 1;
 };
 
-int lsh_help(char **args){
+int ash_help(char **args){
   int i;
   printf("Type program names and arguments, and hit enter.\n");
   printf("The following are built in:\n");
 
-  for(int i; i < lsh_num_builtins(); i++){
+  for(int i; i < ash_num_builtins(); i++){
     printf(" %s\n",builtin_str[i]);
   };
 };
 
-int lsh_exit(char **args){
+int ash_exit(char **args){
   return 0;
 }
 
-int lsh_execute(char **args){
-  int i;
-  
-  if (args[0] == NULL)
-  {
-    return 1;
-  }
-  
-  for (int i; i < lsh_num_builtins(); i++)
-  {
-    if (strcmp(args[0], builtin_str[i]) == 0)
-    {
-      return lsh_launch(args);
-    }
-    
-  }
-  
-
-}
-
-int lsh_launch(char **args)
+int ash_launch(char **args)
 {
   pid_t pid, wpid;
   int status;
@@ -85,12 +65,12 @@ int lsh_launch(char **args)
   {
     if (execvp(args[0], args) == -1)
     {
-      perror("lsh");
+      perror("ash");
     }
     exit(EXIT_FAILURE);
   }else if (pid < 0)
   {
-    perror("lsh");
+    perror("ash");
   } else{
     do
     {
@@ -100,46 +80,46 @@ int lsh_launch(char **args)
   return 1;
 }
 
-char **lsh_split_line(char *line)
+char **ash_split_line(char *line)
 {
-  char bufsize = LSH_TOK_BUFSIZE, position = 0;
+  char bufsize = ASH_TOK_BUFSIZE, position = 0;
   char **tokens = malloc(bufsize * sizeof(char *));
   char *token;
   if (!tokens)
   {
-    fprintf(stderr, "lsh:allocation error\n");
+    fprintf(stderr, "ash:allocation error\n");
     exit(EXIT_FAILURE);
   }
-  token = strtok(line, LSH_TOK_DELIM);
+  token = strtok(line, ASH_TOK_DELIM);
   while (token != NULL)
   {
     tokens[position] = token;
     position++;
     if (position >= bufsize)
     {
-      bufsize += LSH_TOK_BUFSIZE;
+      bufsize += ASH_TOK_BUFSIZE;
       tokens = realloc(tokens, bufsize * sizeof(char *));
       if (!tokens)
       {
-        fprintf(stderr, "lsh:allocation error\n");
+        fprintf(stderr, "ash:allocation error\n");
         exit(EXIT_FAILURE);
       }
     }
-    token = strtok(NULL, LSH_TOK_DELIM);
+    token = strtok(NULL, ASH_TOK_DELIM);
   }
   tokens[position] = NULL;
   return tokens;
 }
 
-char *lsh_read_line(void)
+char *ash_read_line(void)
 {
-  int bufsize = LSH_RL_BUFSIZE;
+  int bufsize = _SC_AVPHYS_PAGES;
   int position = 0;
   char *buffer = malloc(sizeof(char) * bufsize);
   int c;
   if (!buffer)
   {
-    fprintf(stderr, "lsh:allocation error\n");
+    fprintf(stderr, "ash:allocation error\n");
     exit(EXIT_FAILURE);
   }
   while (1)
@@ -157,18 +137,38 @@ char *lsh_read_line(void)
     position++;
     if (position >= bufsize)
     {
-      bufsize += LSH_RL_BUFSIZE;
+      bufsize += ASH_RL_BUFSIZE;
       buffer = realloc(buffer, bufsize);
     }
     if (!buffer)
     {
-      fprintf(stderr, "lsh:allocation error\n");
+      fprintf(stderr, "ash:allocation error\n");
       exit(EXIT_FAILURE);
     }
   }
 }
 
-void lsh_loop(void)
+int ash_execute(char **args){
+  int i;
+  
+  if (args[0] == NULL)
+  {
+    return 1;
+  }
+  
+  for (int i; i < ash_num_builtins(); i++)
+  {
+    if (strcmp(args[0], builtin_str[i]) == 0)
+    {
+      return ash_launch(args);
+    }
+    
+  }
+  
+
+}
+
+void ash_loop(void)
 {
   char *line;
   char **args;
@@ -176,9 +176,9 @@ void lsh_loop(void)
   do
   {
     printf("> ");
-    line = lsh_read_line();
-    args = lsh_split_line(line);
-    status = lsh_execute(args);
+    line = ash_read_line();
+    args = ash_split_line(line);
+    status = ash_execute(args);
 
     free(line);
     free(args);
@@ -187,6 +187,6 @@ void lsh_loop(void)
 
 int main(int argc, char **argv)
 {
-  lsh_loop();
+  ash_loop();
   return EXIT_SUCCESS;
 }
